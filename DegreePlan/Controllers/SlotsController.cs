5e893c5cc3b8fa0d1b8f690a3c2902a11edee994
Susritha <susrithagade1996@>
2019-03-11 15:22:14 -0500

    commiting controllers for StudentTerms,Slot

---
+++ DegreePlan/Controllers/SlotsController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DegreePlan.Data;
+using DegreePlan.Models;
+
+namespace DegreePlan.Controllers
+{
+    public class SlotsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SlotsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Slots
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Slots.ToListAsync());
+        }
+
+        // GET: Slots/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var slot = await _context.Slots
+                .FirstOrDefaultAsync(m => m.SlotId == id);
+            if (slot == null)
+            {
+                return NotFound();
+            }
+
+            return View(slot);
+        }
+
+        // GET: Slots/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Slots/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("SlotId,Status,DegreePlan,Term,CreditId")] Slot slot)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(slot);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(slot);
+        }
+
+        // GET: Slots/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var slot = await _context.Slots.FindAsync(id);
+            if (slot == null)
+            {
+                return NotFound();
+            }
+            return View(slot);
+        }
+
+        // POST: Slots/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("SlotId,Status,DegreePlan,Term,CreditId")] Slot slot)
+        {
+            if (id != slot.SlotId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(slot);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SlotExists(slot.SlotId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(slot);
+        }
+
+        // GET: Slots/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var slot = await _context.Slots
+                .FirstOrDefaultAsync(m => m.SlotId == id);
+            if (slot == null)
+            {
+                return NotFound();
+            }
+
+            return View(slot);
+        }
+
+        // POST: Slots/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var slot = await _context.Slots.FindAsync(id);
+            _context.Slots.Remove(slot);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SlotExists(int id)
+        {
+            return _context.Slots.Any(e => e.SlotId == id);
+        }
+    }
+}
